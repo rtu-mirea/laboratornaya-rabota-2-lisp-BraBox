9899e94fccad90a9bd351b7154f6fbdd1e98944a
Филиппов Роман <qprafaelqp@gmail.com>
2020-05-25 20:19:15 +0300

    lab2

---
+++ Lab2Ex2.lisp
@@ -0,0 +1,8 @@
+(let ((in (open "file.txt" :if-does-not-exist nil)))
+   (when in
+      (loop for line = (read-line in nil)
+      
+      while line do (format t "~a~%" line))
+      (close in)
+   )
+)
