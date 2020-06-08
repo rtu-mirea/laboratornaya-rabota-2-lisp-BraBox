abbb98c65cb22c4562f5e967a4e919d0dc4738e3
BraBox <42098400+BraBox@users.noreply.github.com>
2020-06-08 20:54:59 +0300

    Update Lab2Ex2.lisp

--- Lab2Ex2.lisp
+++ Lab2Ex2.lisp
@@ -1,8 +1,9 @@
-(let ((in (open "file.txt" :if-does-not-exist nil)))
-   (when in
-      (loop for line = (read-line in nil)
-      
-      while line do (format t "~a~%" line))
-      (close in)
-   )
+(defun readFunc(pat_to_file)
+	(let ((in (open path_to_file :if-does-not-exist nil)))
+   		(when in
+      		(loop for line = (read-line in nil)      
+      		while line do (format t "~a~%" line))
+      	(close in)
+   	)
+	)
 )
